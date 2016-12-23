cf6f65497317d03f9067726c9680a03070b71f52
Brian Clark <brian@clark.zone>
2016-12-23 13:08:19 -0500

    composer-related updates

--- 5.6/docker-entrypoint.sh
+++ 5.6/docker-entrypoint.sh
@@ -3,10 +3,27 @@ set -e
 
 UID=${UID:-0}
 GID=${GID:-0}
+home=/root
 
-# If we are running composer, make sure it executes as the specified uid/gid.
+# if running composer, support custom uid/gid and ssh
 if [ "$1" = "composer" ]; then
-  su-exec ${UID}:${GID} "$@"
+  if [ "$UID" -ne "0" ]; then
+    home=/home/composer
+    addgroup -g $GID composer
+    adduser -D -u $UID -h $home -G composer composer
+  fi
+
+  if test -f /ssh/id_rsa; then
+    echo "ssh key detected, installing ssh..."
+    apk add --no-cache openssh-client
+    mkdir -p $home/.ssh
+    cp /ssh/id_rsa $home/.ssh/id_rsa
+    chmod 600 $home/.ssh/id_rsa
+    test -f /ssh/known_hosts && ln -s /ssh/known_hosts $home/.ssh/
+  fi
+
+  chown -R $UID:$GID $home
+  exec su-exec $UID:$GID "$@"
 fi
 
 exec "$@"
